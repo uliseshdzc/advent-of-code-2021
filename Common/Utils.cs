c6b8c4eb656e34ab224ebf8d3bfb2e40711a0a1a
Ulises Hernández Calzadillas <134593884+uliseshdzc@users.noreply.github.com>
2024-07-30 13:35:05 +0200

    Day 01 challenge

---
+++ Common/Utils.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace Common;
+public static class Utils
+{
+    private const string SessionCookieParameter = "SessionCookie";
+    private const string YearParameter = "Year";
+
+    public static async Task<string> GetInput(int day)
+    {
+        var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        var configuration = configurationBuilder.Build();
+
+        using var client = new HttpClient();
+        int year = int.Parse(configuration[YearParameter] 
+            ?? throw new ConfigurationErrorsException($"Error while loading the parameter {YearParameter} from the configuration file."));
+        string sessionCookieValue = configuration[SessionCookieParameter] 
+            ?? throw new ConfigurationErrorsException($"Error while loading the parameter {SessionCookieParameter} from the configuration file.");
+
+        var url = $"https://adventofcode.com/{year}/day/{day}/input";
+            
+        client.DefaultRequestHeaders.Add("Cookie", $"session={sessionCookieValue}");
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsStringAsync();
+}
+}
+
---
+++ Day01/Program.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+
+static int CountMeasurementIncreases(IEnumerable<int> measurements)
+    => measurements
+    .Skip(1)
+    .Zip(measurements, (a, b) => a > b)
+    .Count(x => x);
+
+string input = await Utils.GetInput(day: 1);
+
+var measurements = input.Trim()
+    .Split("\n")
+    .Select(int.Parse)
+    .ToList();
+
+var firstResult = CountMeasurementIncreases(measurements);
+Console.WriteLine(firstResult);
+
+var slidingWindowMeasurements = Enumerable
+    .Range(0, measurements.Count - 2)
+    .Select(i => measurements[i] + measurements[i + 1] + measurements[i + 2]);
+
+var secondResult = CountMeasurementIncreases(slidingWindowMeasurements);
+Console.WriteLine(secondResult);

d5235d9363eaec4feea5800028cc83f102b59ace
Ulises Hernández Calzadillas <134593884+uliseshdzc@users.noreply.github.com>
2024-08-01 10:59:17 +0200

    Day 07 challenge

---
+++ Day07/Program.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+
+string input = await Utils.GetInput(day: 7);
+
+List<int> positions = input.Split(',').Select(int.Parse).ToList();
+
+var minDistance = int.MaxValue;
+for (int i = positions.Min(); i <= positions.Max(); i++)
+{
+    minDistance = Math.Min(
+        positions.Select(c => Math.Abs(c - i)).Sum(),
+        minDistance);
+
+}
+
+Console.WriteLine(minDistance);
+
+static int ConsecutiveSum(int x) => x * (x + 1) / 2;
+
+minDistance = int.MaxValue;
+for (int i = positions.Min(); i <= positions.Max(); i++)
+{
+    minDistance = Math.Min(
+        positions.Select(c => ConsecutiveSum(Math.Abs(c - i))).Sum(),
+        minDistance);
+}
+
+
+Console.WriteLine(minDistance);
